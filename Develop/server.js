f1901977dca787f40ef53c116d2396a002e51706
Alan Fernandes <anoblefernandes@gmail.com>
2020-12-18 14:39:43 -0800

    Final code chages to app.

--- Develop/server.js
+++ Develop/server.js
@@ -2,6 +2,8 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
+var fs = require("fs")
+var uuid = require("uuid")
 
 // Sets up the Express App
 // =============================================================
@@ -15,15 +17,54 @@ app.use(express.json());
 app.use(express.static('public'))
 
 app.get('/notes',function(req, res){
-    var notes = [
-        {text: "Notes are useful", id: 1},
-        {text: "Notes are fun", id: 2},
-        {text: "Notes are good", id: 3},
-    ]
-    res.json(notes)
+ 
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 })
 
+
+app.get('/api/notes', function(req, res){
+    fs.readFile("./db/db.json","utf-8", (err, data)=>{
+        if(err)throw err
+        console.log(data)
+        res.json(JSON.parse(data))
+    })
+
+})
+app.post('/api/notes', function(req, res){
+    fs.readFile("./db/db.json","utf-8", (err, data)=>{
+        if(err)throw err
+        var db = JSON.parse(data)
+        // res.json(JSON.parse(data))
+        var newnote = {
+            title: req.body.title,
+            text: req.body.text,
+            id: uuid.v1()
+        }
+        db.push(newnote)
+        fs.writeFile("./db/db.json", JSON.stringify(db), (err, data) =>{
+            if (err) throw err
+            res.json(db)
+        })
+    })
+
+})
+app.delete('/api/notes/:id', function(req, res){
+    fs.readFile("./db/db.json","utf-8", (err, data)=>{
+        if(err)throw err
+        var db = JSON.parse(data)
+        var id = req.params.id
+        db = db.filter(note => note.id!==id)
+        fs.writeFile("./db/db.json", JSON.stringify(db), (err, data) =>{
+            if (err) throw err
+            res.json(db)
+        })
+    })
+
+})
+app.get('*',function(req, res){
+ 
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
